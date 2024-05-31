372f39a0531765136d15dcda91aaeb244767f14d
Sai <147521543+saimav007@users.noreply.github.com>
2024-05-31 21:02:47 +0530

    Create theory2.v

---
+++ theory2.v
@@ -0,0 +1,5 @@
+A24)
+`define SIZE 40 : The value can be passed during run time also. But it can not be passed
+as argument during instantiations.
+parameter SIZE 40: The value can not be changed during run time. It can be passed as
+argument along with module instantiations.
