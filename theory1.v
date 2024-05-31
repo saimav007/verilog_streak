d9f47b965b2b95046a40f0792045a14a6a0baf20
Sai <147521543+saimav007@users.noreply.github.com>
2024-05-31 17:43:56 +0530

    Create theory1.v

---
+++ theory1.v
@@ -0,0 +1,5 @@
+What do x and y specify in the following verilog statement?
+`timescale x / y
+
+x is the unit in which the times and delays are reperesented (genrally its nanoseconds)
+y is the unit in which the precision of rounding off of the delays are represented(generally its picoseconds)
