0b5fdd45ba24ca22860d1c1b6e4657defd2bcae1
Sai <147521543+saimav007@users.noreply.github.com>
2024-05-31 18:16:33 +0530

    Create theory3.v

---
+++ theory3.v
@@ -0,0 +1,5 @@
+Q) what is casex and casex
+
+casex and casez are the special versions of the case statements allow the X and Z logic
+values to be used as don’t cares. casez treats all bit positions with z as a don't cares.
+Casex treats all x and z values in case alternative as a don't cares.
