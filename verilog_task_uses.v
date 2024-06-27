ecf7636b6952a051e028f65ea6baaa648dc9e4d9
Sai <147521543+saimav007@users.noreply.github.com>
2024-06-27 19:21:53 +0530

    Create verilog_task_uses.v

---
+++ verilog_task_uses.v
@@ -0,0 +1,5 @@
+$display, $monitor, $strobe	Prints the specified message or value to the console or waveform viewer during simulation.
+$fopen, $fscanf, $fclose	File operations to open, scan lines and close files
+$random	Generates a random value within the specified range.
+$stop, $finish	Simulation termination commands to stop/exit execution
+$readmemb, $readmemh	Reads memory contents from a file into a Verilog array.
