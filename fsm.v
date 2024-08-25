30bf3016b00fe6f93039a4390fca8f804c279bfd
Sai <147521543+saimav007@users.noreply.github.com>
2024-08-25 11:42:09 +0530

    Update fsm.v

--- fsm.v
+++ fsm.v
@@ -1,93 +1,100 @@
-module mealy11011(in,clk,rst,out,ps,ns);
-input in,clk,rst;
-output reg out,ps,ns;
-parameter s=0,s1=1,s11=2,s110=3,s1101=4,s11011=5;
-always @(posedge clk)begin
-if(rst)begin
+module mealy(clk,rst,in,out,ps,ns);
+parameter reset=0,s0=1,s01=2,s011=3;
+input clk,rst,in;
+output reg [2:0]ps,ns;
+output reg out;
+
+always@(posedge clk)begin
+if(rst)begin 
 out<=0;
 end
-else begin
+else begin 
+ps<=ns;
+end
+end
+
+always@(ps or in)begin
 case(ps)
 
-s:begin
-ns<=in?s1:s;
+reset: begin 
+ns<=in?reset:s0;
 out<=0;
 end
 
-s1:begin
-ns<=in?s11:s;
+s0:begin
+ns<=in? s01:s0;
 out<=0;
 end
 
-s11:begin
-ns<=in?s11:s110;
+s01:begin 
+ns<=in? s011:s0;
 out<=0;
 end
 
-s110:begin
-ns<=in?s1101:s;
+s011:begin 
+ns<=in?reset:s0;
+out<=in?0:1;
+end
+
+default: begin 
+ns<=reset;
 out<=0;
 end
 
-s1101:begin
-ns<=in?s11:s;
-out<=0;
+endcase
 end
-
-default:begin
-ns<=s;
-out<=0;
-end
-
-endcase 
-end 
-end
-
 endmodule
 
 module test();
-reg in,clk,rst;
-wire out,ps,ns;
-mealy11011 DUT(.in(in),.clk(clk),.rst(rst),.out(out),.ps(ps),.ns(ns));
-initial begin 
-clk=0;
-forever#2  clk=~clk;
-end
+reg clk,rst,in;
+wire out;
+wire [2:0]ps,ns;
+mealy m1(clk,rst,in,out,ps,ns);
 initial begin
-rst=1;
+ clk=0;
+ forever #5 clk=~clk;
+ end
+ 
+ initial begin
 #10;
-rst=0;
-#10
-in=1;
-#5;
-in=1;
-#5;
-in=0;
-#5;
-in=1;
-#5;
-in=1;
-#5;
-in=0;
-#5;
-in=1;
-#5;
-in=1;
-#5;
-in=0;
-#5;
-in=1;
-#5;
-in=1;
-#5;
-in=0;
-#5;
-# 100 $stop;
-end
-initial begin
-$monitor("time=%d,in=%b,clk=%b,rst=%b,ps=%b,ns=%b,out=%b",$time,in,clk,rst,ps,ns,out);
-     $dumpfile("mux81.vcd");
-     $dumpvars();
+ rst=0;
+ #5;
+ in=1;
+ #5;
+  in=0;
+ #5;
+  in=0;
+ #5;
+  in=1;
+ #5;
+  in=1;
+ #5;
+  in=0;
+ #5;
+  in=1;
+ #5;
+  in=1;
+ #5;
+  in=0;
+ #5;
+  in=1;
+ #5;
+  in=0;
+ #5;
+  in=1;
+ #5;
+  in=1;
+ #5;
+  in=0;
+ #5;
+ #20 $stop();
+ end
+ 
+ initial begin
+ $monitor("time=%t, clk=%d,rst=%d,in=%d,ps=%d,ns=%d,out=%d",$time,clk,rst,in,ps,ns,out);
+ $dumpfile("mealy.vcd");
+ $dumpvars();
  end
  
  endmodule
+ 
